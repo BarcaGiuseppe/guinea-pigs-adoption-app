1d680161315d72627ef373d43a36220e0dcc4180
BarcaGiuseppe <gbarca1995@gmail.com>
2024-04-04 10:36:33 +0200

    feat(ui): added navbar component to app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,12 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { ContextProvider } from "@/ContextProvider";
+import Navbar from "@/components/Navbar";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ContextProvider>
+      <Navbar></Navbar>
       {/* Wrappo la radice della nostra app così da avere accesso ad ogni componente*/}
       <Component {...pageProps} />
     </ContextProvider>
