ef673efdd9ca381cd4fce3b4eddb9ca3affb7634
BarcaGiuseppe <gbarca1995@gmail.com>
2024-04-04 11:09:49 +0200

    feat(ui): added footer component to app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,13 +2,15 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { ContextProvider } from "@/ContextProvider";
 import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ContextProvider>
-      <Navbar></Navbar>
+      <Navbar />
       {/* Wrappo la radice della nostra app così da avere accesso ad ogni componente*/}
       <Component {...pageProps} />
+      <Footer />
     </ContextProvider>
   );
 }
